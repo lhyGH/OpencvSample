1c2f5dff12b7a7fc9047ca275e2befbb9b92e356
LHY <1443903485@qq.com>
2020-09-18 15:24:12 +0800

    1

---
+++ OpencvSamples/Samples/ReadRTSP.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpencvSamples.Samples
+{
+    public class ReadRTSP : ISample
+    {
+        public void Run()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
